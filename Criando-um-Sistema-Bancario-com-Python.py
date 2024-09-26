2656cfe6780fe4e2e8561457b3efa1da5ca8705d
LabLearnie <lablearnie@gmail.com>
2024-09-26 19:38:41 -0300

    Add files via upload

---
+++ Criando-um-Sistema-Bancario-com-Python.py
@@ -0,0 +1,64 @@
+main_MENU = """ \n 
+<<<<<<<<<<<<<<<<<<  DIO Bank  >>>>>>>>>>>>>>>>>>
+( d ) Depositar
+( s ) Sacar
+( e ) Extrato
+( q ) Sair
+>>  """
+
+saldo = 0
+limite = 3500
+extrato = ""
+numero_saques = 0
+limite_saques = 3
+
+while True:
+    opcao = input(main_MENU)    
+
+    if opcao == "d":
+        valor = float(input("Valor a ser depositado:  "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f} \n"
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+    
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque:  "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= limite_saques
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente para realizar este saque.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O Valor do saque excede seu limite de cheque-epecial")
+        
+        elif excedeu_saques:
+            print("Operação falhou! Você excedeu o limite de saques diários.")
+        
+        elif valor > 0:
+            saldo -= valor 
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+
+    elif opcao == "e":
+        print("\n <<<<<<<<<  EXTRATO  >>>>>>>>>")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>")
+
+    elif opcao == "q":
+        break
+
+    else: 
+        print("Operação inválida! Por favor selecione novamente.")
